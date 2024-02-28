223393487a80264e443f80d6d0836381d9506bf1
yusukeyurameshi <fabio@yusuketi.com>
2024-02-28 14:02:58 -0300

    .

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 sudo yum -y install git.x86_64 oracle-instantclient-release-el8.x86_64 
-sudo yum -y install oracle-instantclient-basic.x86_64 
-#oracle-instantclient-sqlplus.x86_64
+sudo yum -y install oracle-instantclient-basic.x86_64 oracle-instantclient-sqlplus.x86_64
 cd
 git clone https://github.com/yusukeyurameshi/Labs.git
 
