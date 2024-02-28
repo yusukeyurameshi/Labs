1a3436a3cea898253ea6011c672568875c851801
yusukeyurameshi <fabio@yusuketi.com>
2024-02-28 13:51:16 -0300

    .

--- setup.sh
+++ setup.sh
@@ -2,25 +2,19 @@ sudo yum -y install git.x86_64 oracle-instantclient-release-el8.x86_64
 cd
 git clone https://github.com/yusukeyurameshi/Labs.git
 
+cd Labs
+
+echo -n "DB Host: "
+read -r DBHost
+
+echo -n "DBA User: "
+read -r dbauser
+
+echo -n "Password: "
+read -r Password
 
 
-echo "Hello, please introduce yourself."
-
-echo -n "Your name: "
-read -r name
-
-echo "Are you human?"
-
-echo -n "y/n: "
-read -r human
-
-echo "What is your favorite programming language?"
-
-echo -n "Your answer: "
-read -r lang
-
-echo
-echo "Answers:"
-echo "1. $name"
-echo "2. $human"
-echo "3. $lang"
+echo "[DEFAULT]
+DBHost = ${DBHost}
+dbauser = ${dbauser}
+Password = ${Password}">.config
