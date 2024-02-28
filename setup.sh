e018c3993f75bdb9e88f91a516b4d0f016200484
yusukeyurameshi <fabio@yusuketi.com>
2024-02-28 14:36:24 -0300

    .

--- setup.sh
+++ setup.sh
@@ -31,5 +31,5 @@ dbauser = ${dbauser}
 Password = ${Password}
 ServiceName = ${ServiceName}">.config
 
-sqlplus -s ${dbauser}/${Password}@${DBHost}:1521/${ServiceName} @cleanup.sql
-sqlplus -s ${dbauser}/${Password}@${DBHost}:1521/${ServiceName} @install.sql
+echo "sqlplus -s ${dbauser}/${Password}@${DBHost}:1521/${ServiceName} @cleanup.sql"
+echo "sqlplus -s ${dbauser}/${Password}@${DBHost}:1521/${ServiceName} @install.sql"
